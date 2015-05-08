b2c5d9ede98e7843b47671b9281d5e6f7831883b
취업반PM <PM@itbank_12-PC>
2015-05-08 21:35:43 +0900

    구구단을 외우자라고 명령을 주었음

--- hello/src/hello/Hello.java
+++ hello/src/hello/Hello.java
@@ -2,6 +2,9 @@ package hello;
 
 class Hello {
 	public static void main(String[] args) { 
+		
+		System.out.println("구구단을 외우자");
+		
 		for (int i = 2; i < 10; i+=4) {
 			for (int j = 1; j < 10; j++) {
 				System.out.println(
